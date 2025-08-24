0bfa4c6d89f2556e2b5f93aa611f76c41be6809c
Sherlock <playyala@yahoo.com>
2025-08-24 08:05:33 +0300

    add NoNumberAttribute in validations folder

---
+++ ITIManagement.BLL/Validations/NoNumbersAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITIManagement.BLL.Validations
+{
+	public class NoNumbersAttribute : ValidationAttribute
+	{
+		public NoNumbersAttribute()
+		{
+			ErrorMessage = "{0} cannot contain numbers.";
+		}
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value is string str && str.Any(char.IsDigit))
+			{
+				return new ValidationResult(string.Format(ErrorMessage, validationContext.DisplayName));
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
